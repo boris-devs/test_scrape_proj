dd95d46457f4e34eaf6f60caa2c3113aad44377a
xborismenx <boris.tikhonov.21@gmail.com>
2026-02-26 00:30:07 +0200

    Update product detail URL to accept string external_id

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,5 @@ from products.views import ListProductsTrackingView, ProductDetailView
 
 urlpatterns = [
     path("tracking/",  ListProductsTrackingView.as_view(), name="list-products-tracking"),
-    path("tracking/<int:external_id>/",  ProductDetailView.as_view(), name="list-products-tracking"),
+    path("tracking/<str:external_id>/",  ProductDetailView.as_view(), name="list-products-tracking"),
 ]
